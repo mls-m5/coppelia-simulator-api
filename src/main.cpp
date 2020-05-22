746d32f8375b62283759f13cc84766dfa8523b70
Mattias Lasersköld <mattias.l.sk@gmail.com>
2020-05-22 18:48:11 +0200

    Fixed threading in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,10 @@
 
 #include "gui.h"
 #include "hexapod.h"
-#include <unistd.h>
 #include <csignal>
 #include <cstdlib>
+#include <thread>
+#include <unistd.h>
 
 namespace {
 
@@ -32,38 +33,34 @@ void proxSensor_CB(std::vector<msgpack::object> *msg) {
     printf(".");
 }
 
-std::vector<Hexapod*> createHexapods(int numHexapods)
-{
-    std::vector<Hexapod*> hexapods;
+std::vector<Hexapod *> createHexapods(int numHexapods) {
+    std::vector<Hexapod *> hexapods;
     int hexapodNum = 0;
-    for (int i = 0; i < numHexapods; i++)
-    {
+    for (int i = 0; i < numHexapods; i++) {
         hexapods.push_back(new Hexapod(client.get(), hexapodNum++));
     }
     return hexapods;
 }
 
-
 int main(int argc, char *argv[]) {
     Gui gui(argc, argv);
 
-    auto stopSim = [] ()
-    {
+    auto stopSim = []() {
         // Stop simulation
         std::cout << "Ended!" << std::endl;
-        b0RemoteApi::print(client->simxStopSimulation(client->simxServiceCall()));
-        //exit(0);
+        b0RemoteApi::print(
+            client->simxStopSimulation(client->simxServiceCall()));
+        // exit(0);
     };
 
     std::atexit(stopSim);
-    //std::signal(SIGINT, SIG_DFL);
-    //std::signal(SIGABRT, stopSim);
-    //std::signal(SIGTERM, stopSim);
-    //std::signal(SIGTSTP, stopSim);
+    // std::signal(SIGINT, SIG_DFL);
+    // std::signal(SIGABRT, stopSim);
+    // std::signal(SIGTERM, stopSim);
+    // std::signal(SIGTSTP, stopSim);
 
     std::cout << "program started" << std::endl;
-    client = std::make_unique<b0RemoteApi>("b0RemoteClient","b0RemoteApi");
-
+    client = std::make_unique<b0RemoteApi>("b0RemoteClient", "b0RemoteApi");
 
     stopSim(); // Start with stopping the sim, if any active
 
@@ -74,9 +71,10 @@ int main(int argc, char *argv[]) {
 
     // Start the simulation
     b0RemoteApi::print(client->simxStartSimulation(client->simxServiceCall()));
-    
+
     char msg[20] = "testmsg to hexapod";
-    client->simxSetStringSignal("test", msg, sizeof(msg), client->simxServiceCall());
+    client->simxSetStringSignal(
+        "test", msg, sizeof(msg), client->simxServiceCall());
 
     // Change heading
     hexapods.at(0)->setTargetHeading(75);
@@ -84,13 +82,13 @@ int main(int argc, char *argv[]) {
     while(hexapods[0].run())
     {
         usleep(100);
-    } 
+    }
     hexapods[0].setTargetHeading(-75);
     while(hexapods[0].run())
     {
         usleep(100);
-    } 
-    
+    }
+
     hexapods[0].walk(0.2, 0.0);
     hexapods[1].walk(0.5, 0.3);
     sleep(5);
@@ -98,26 +96,30 @@ int main(int argc, char *argv[]) {
 
     std::cout << "done" << std::endl;
 
-    for (auto hexapod : hexapods)
-    {
-        hexapod->navigate(0,0);
+    for (auto hexapod : hexapods) {
+        hexapod->navigate(0, 0);
     }
+
+    bool abort = false;
 
     // Finish all jobs
-    while (bool jobLeft = true)
-    {
-        bool someNotDone = false;
-        for (auto hexapod : hexapods)
-        {
-            if (!hexapod->run())
-            {
-                someNotDone = true;
+    auto jobThread = std::thread([&]() {
+        while (bool jobLeft = true && !abort) {
+            bool someNotDone = false;
+            for (auto hexapod : hexapods) {
+                if (!hexapod->run()) {
+                    someNotDone = true;
+                }
             }
+            jobLeft = !someNotDone;
         }
-        jobLeft = !someNotDone;
-    }
+    });
 
     gui.mainLoop();
+
+    abort = true;
+
+    jobThread.join();
 
     return (0);
 }
